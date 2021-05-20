d21b27ebee24809631c6fc43e667ff4832c51ae7
Tristan Igelbrink <t.igelbrink@kalverkamp.de>
2021-05-20 13:14:01 +0000

    fixed cov

--- livox_ros2_driver/livox_ros2_driver/lddc.cpp
+++ livox_ros2_driver/livox_ros2_driver/lddc.cpp
@@ -492,16 +492,16 @@ uint32_t Lddc::PublishImuData(LidarDataQueue *queue, uint32_t packet_num,
   imu_data.angular_velocity.x = imu->gyro_x;
   imu_data.angular_velocity.y = imu->gyro_y;
   imu_data.angular_velocity.z = imu->gyro_z;
-  imu_data.linear_acceleration_covariance = [ cov_imu, 0, 0,
-                                              0, cov_imu, 0,
-                                              0, 0, cov_imu ];
+  imu_data.angular_velocity_covariance[0] = cov_imu;
+  imu_data.angular_velocity_covariance[5] = cov_imu;
+  imu_data.angular_velocity_covariance[8] = cov_imu;
 
   imu_data.linear_acceleration.x = imu->acc_x;
   imu_data.linear_acceleration.y = imu->acc_y;
   imu_data.linear_acceleration.z = imu->acc_z;
-  imu_data.linear_acceleration_covariance = [ cov_imu, 0, 0,
-                                              0, cov_imu, 0,
-                                              0, 0, cov_imu ];
+  imu_data.linear_acceleration_covariance[0] = cov_imu;
+  imu_data.linear_acceleration_covariance[5] = cov_imu;
+  imu_data.linear_acceleration_covariance[8] = cov_imu;
 
 
   QueuePopUpdate(queue);
